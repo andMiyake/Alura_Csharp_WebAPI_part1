898f118aef343466200ce529a41232df16d1f7af
Anderson Felipe Miyake <319013@ptin.corppt.com>
2021-12-06 21:58:00 -0300

    First commit

---
+++ Alura.WebAPI.Api/Controllers/LivrosController.cs
@@ -0,0 +1,100 @@
+﻿using Alura.ListaLeitura.Modelos;
+using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.Api.Controllers
+{
+    [Authorize]
+    [ApiController] //Atributo para identificar que é um Controlador para servir a API
+    [Route("api/[controller]")] //nome do controlador
+    public class LivrosController : ControllerBase
+    {
+        public readonly IRepository<Livro> _repo;
+
+        public LivrosController(IRepository<Livro> repository)
+        {
+            _repo = repository;
+        }
+
+        [HttpGet]
+        public IActionResult ListaDeLivros()
+        {
+            var lista = _repo.All.Select(l => l.ToApi()).ToList();
+            return Ok(lista);
+        }
+
+        [HttpGet("{id}")]   //Indicando o segundo segmento para a Action
+        public IActionResult Recuperar(int id)
+        {
+            var model = _repo.Find(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            return Ok(model.ToApi());
+        }
+
+        [HttpGet("{id}/capa")]
+        public IActionResult ImagemCapa(int id)
+        {
+            byte[] img = _repo.All
+                .Where(l => l.Id == id)
+                .Select(l => l.ImagemCapa)
+                .FirstOrDefault();
+            if (img != null)
+            {
+                return File(img, "image/png");
+            }
+            return File("~/images/capas/capa-vazia.png", "image/png");
+        }
+
+        [HttpPost]
+        public IActionResult Incluir([FromForm] LivroUpload model)
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                _repo.Incluir(livro);
+                var uri = Url.Action("Recuperar", new { id = livro.Id });
+                return Created(uri, livro); //201
+            }
+            return BadRequest();
+        }
+
+        [HttpPut]
+        public IActionResult Alterar([FromForm] LivroUpload model)
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                if (model.Capa == null)
+                {
+                    livro.ImagemCapa = _repo.All
+                        .Where(l => l.Id == livro.Id)
+                        .Select(l => l.ImagemCapa)
+                        .FirstOrDefault();
+                }
+                _repo.Alterar(livro);
+                return Ok();    //200
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remover(int id)
+        {
+            var model = _repo.Find(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            _repo.Excluir(model);
+            return NoContent(); //204
+        }
+    }
+}
---
+++ Alura.WebAPI.Api/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alura.ListaLeitura.Api.Formatters;
+using Alura.ListaLeitura.Modelos;
+using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Alura.WebAPI.Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<LeituraContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("ListaLeitura"));
+            });
+
+            services.AddTransient<IRepository<Livro>, RepositorioBaseEF<Livro>>();
+
+            services.AddMvc(options =>
+            {
+                options.OutputFormatters.Add(new LivroCsvFormatter());
+            }).AddXmlSerializerFormatters();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "JwtBearer";    //Esquema de autentificação padrão
+                options.DefaultChallengeScheme = "JwtBearer";
+            })
+            .AddJwtBearer("JwtBearer", options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,      //Validando criador do Token
+                    ValidateAudience = true,    //Validando quem pedi o token
+                    ValidateLifetime = true,    //Validando a expiração
+                    ValidateIssuerSigningKey = true,    //Validando chave
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("alura-webapi-authentication-valid")),   //Chave de Assinatura usada pelo Issuer, chave usada para validar no Juiz
+                    ClockSkew = TimeSpan.FromMinutes(5),
+                    ValidIssuer = "Alura.WebApp",
+                    ValidAudience = "Postman",
+                };
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
